f9352571f8a1bd82ed8735b9044b8a14f9eed922
Natan Derick <114193508+NatanXT@users.noreply.github.com>
2024-10-03 14:36:14 -0300

    Testes

--- APP-Financeiro/src/app/app.routes.ts
+++ APP-Financeiro/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import {Routes} from '@angular/router';
-import {HomeComponent} from './home/home.component';
+import {HomeComponent} from './componentes/home/home.component';
+import {GrupoListaComponent} from './componentes/grupo/grupo-lista/grupo-lista.component';
 
 export const routes: Routes = [
 {path: '', redirectTo: '/home', pathMatch: 'full'},
-{path: 'home', component: HomeComponent}
+{path: 'home', component: HomeComponent},
+{path: 'teste', component: GrupoListaComponent}
 ];
--- APP-Financeiro/src/app/componentes/grupo/grupo-lista/grupo-lista.component.ts
+++ APP-Financeiro/src/app/componentes/grupo/grupo-lista/grupo-lista.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
-
+impo
 @Component({
   selector: 'app-grupo-lista',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonModule,
+                TagModule,
+                CommonModule,
+                CardModule,
+                TableModule,
+                RouterModule,
+                DialogModule,
+                FormsModule,
+                ToastModule,
+                InputTextModule,
+                SidebarComponent,
+                ConfirmDialogModule,
+                FloatLabelModule,
+                DropdownModule,],
   templateUrl: './grupo-lista.component.html',
   styleUrl: './grupo-lista.component.css'
 })
