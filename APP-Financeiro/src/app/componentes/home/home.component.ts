ec77d455923412b60b9b47d858a47c604d9e8fc5
Natan Derick <114193508+NatanXT@users.noreply.github.com>
2024-10-04 17:26:59 -0300

    Entrega Projeto
    Falta arrumar bastante erros tanto na conexão com o PrimeNG quanto com o repositório, mas por enquanto por mim é uma boa entrega

--- APP-Financeiro/src/app/componentes/home/home.component.ts
+++ APP-Financeiro/src/app/componentes/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BarraderolarComponent } from './barraderolar/barraderolar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [BarraderolarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- APP-Financeiro/src/app/types/pessoa.types.ts
+++ APP-Financeiro/src/app/types/pessoa.types.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 export class PessoaService {
 
   constructor() { }
+}
+
+export interface IPessoa {
+  id: number | null;
+  nome: string;
+  cpf: string ;
+  email:string;
+  telefone:string;
 }
